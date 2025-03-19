0079b18d63837bc041cc0f79aa1aa14851fa464b
Michael Abramowski <mabramowski3@gmail.com>
2025-03-19 14:31:14 -0500

    update folder structure

--- packages/core/src/test/index.test.ts
+++ packages/core/src/test/index.test.ts
@@ -1,11 +1,10 @@
-import { expect, test, vi } from "vitest";
+import { expect, test } from "vitest";
 import { updateActiveStatus } from "..";
-
 
 test("should return the same status as the one passed as an argument", async () => {
   const expected = true;
   const mockDbClient = {
-    send: (args) =>  Promise.resolve({Attributes: {active: true}})
+    send: (args: any) =>  Promise.resolve({Attributes: {active: true}})
   }
   const result = await updateActiveStatus(mockDbClient as any, "Advil", true);
   console.log(result);
